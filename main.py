5ebfa5cadbdfd4e1ea79c01cde8d0594305f006b
Alex Shiu <sysalex0@gmail.com>
2024-05-27 19:43:00 +0800

    update

--- main.py
+++ main.py
@@ -9,26 +9,23 @@ execution_date = datetime.datetime.now()
 stocks_df = get_all_stocks()
 valid_ticker_stocks_df = stocks_df[stocks_df['symbol'].str.fullmatch('[A-Z]+')]
 
-screeners = [
-    MarkMinerviniStockScreener(date=execution_date, stocks_df=valid_ticker_stocks_df)
-]
+screener = MarkMinerviniStockScreener(date=execution_date, stocks_df=valid_ticker_stocks_df)
 
-for screener in screeners:
-    screened_stocks_df = screener.screen()
+screened_stocks_df = screener.screen()
 
-    pre_screened_stocks_df = screener._applied_criteria_stocks_df
-    pre_screened_filename = 'output/criteria/{}_pre_screened_stocks_for_{}_at_{}.csv'.format(
-        screener.get_screener_name(),
-        execution_date.strftime('%Y%m%d'),
-        execution_date.strftime('%Y%m%d%H%M%S')
-    )
-    create_directory_if_not_exist(pre_screened_filename)
-    pre_screened_stocks_df.to_csv(pre_screened_filename)
+pre_screened_stocks_df = screener._applied_criteria_stocks_df
+pre_screened_filename = 'output/criteria/{}_pre_screened_stocks_for_{}_at_{}.csv'.format(
+screener.get_screener_name(),
+execution_date.strftime('%Y%m%d'),
+execution_date.strftime('%Y%m%d%H%M%S')
+)
+create_directory_if_not_exist(pre_screened_filename)
+pre_screened_stocks_df.to_csv(pre_screened_filename)
 
-    screened_stocks_filename = 'output/{}_screened_stocks_for_{}_at_{}.csv'.format(
-        screener.get_screener_name(),
-        execution_date.strftime('%Y%m%d'),
-        execution_date.strftime('%Y%m%d%H%M%S')
-    )
-    create_directory_if_not_exist(screened_stocks_filename)
-    screened_stocks_df.to_csv(screened_stocks_filename)
+screened_stocks_filename = 'output/{}_screened_stocks_for_{}_at_{}.csv'.format(
+screener.get_screener_name(),
+execution_date.strftime('%Y%m%d'),
+execution_date.strftime('%Y%m%d%H%M%S')
+)
+create_directory_if_not_exist(screened_stocks_filename)
+screened_stocks_df.to_csv(screened_stocks_filename)
